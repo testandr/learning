7b30912dff795f883b34e90df83af0e6e6e0c581
testandr <testandr31@gmail.com>
2021-08-27 14:19:48 +0300

    homework 6

---
+++ src/main/java/javaTestersL6/Main.java
@@ -0,0 +1,43 @@
+package javaTestersL6;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Main {
+    public static final String HOST = "dataservice.accuweather.com";
+    public static final String FORECAST_URL = "forecasts";
+    public static final String DAILY_URL = "daily";
+    public static final String FIVE_DAYS_URL = "5day";
+    public static final String API_V = "v1";
+    public static final String CITY_ID = "2-242405_1_AL";
+    public static final String API_KEY = "TpzDN4DaUsgaVRgUrDmK6ho2Gbj7Kw5L";
+
+
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host(HOST)
+                .addPathSegment(FORECAST_URL)
+                .addPathSegment(API_V)
+                .addPathSegment(DAILY_URL)
+                .addPathSegment(FIVE_DAYS_URL)
+                .addPathSegment(CITY_ID)
+                .addQueryParameter("apikey", API_KEY)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(Objects.requireNonNull(response.body()).string());
+
+    }
+}
